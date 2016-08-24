48b2c5b4e5957ea48d3571d6f9fb06786ae809ce
pasca <pasca@DESKTOP-QNCN2TH.fritz.box>
2016-08-24 19:46:45 +0200

    so finally everthing git has to be cleaned up

--- src/com/pascalstieber/mrlocksmith/order/OrderEntity.java
+++ src/com/pascalstieber/mrlocksmith/order/OrderEntity.java
@@ -1,5 +1,27 @@
 package com.pascalstieber.mrlocksmith.order;
 
+<<<<<<< Upstream, based on origin/master
 public class OrderEntity {
 
+=======
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class OrderEntity {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    
+>>>>>>> 235b76e - wildfly maven integration completet - visual changes in gui - added database facets - exported run_configurations - renamed config.cli to jboss_config.cli
 }
